4e437800a45788585b7be9af01c9aca3d125ede0
Justin Yee <jyee048@ucr.edu>
2024-02-13 20:57:55 -0800

    Test water mask and better line detection

--- parallel_lines.py
+++ parallel_lines.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-#Find and compare slopes of two lines
+# Find and compare slopes of two lines
 def parallel(line1, line2):
     x1, y1, x2, y2 = line1[0]
     if (x2 - x1) != 0:
-        slope1 = (y2 - y1) / (x2 - x1) 
-    else: 
+        slope1 = (y2 - y1) / (x2 - x1)
+    else:
         slope1 = float('inf')
 
     x1, y1, x2, y2 = line2[0]
@@ -17,66 +17,68 @@ def parallel(line1, line2):
 
     return (abs(slope1 - slope2) < 0.5 or slope1 == slope2) and (abs(slope1) > 5 and abs(slope2) > 5)
 
-#Draw line on image
+# Draw line on image
 def drawLine(line):
     x1, y1, x2, y2 = line[0]
     cv2.line(final, (x1, y1), (x2, y2), (0, 0, 255), 5)
+    drawn_lines.add(tuple(line[0]))  # Add the drawn line to a set
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture('videoplayback.mp4')
- 
+cap = cv2.VideoCapture('robosub 2022 gate only.mp4')
+
 # Check if camera opened successfully
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
- 
+if (cap.isOpened() == False):
+    print("Error opening video stream or file")
+
 # Read until video is completed
-while(cap.isOpened()):
-  # Capture frame-by-frame
-  ret, frame = cap.read()
-  if ret == True:
-    # Read in as grayscale
-    gray =  cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+drawn_lines = set()  # Set to store already drawn lines
+while (cap.isOpened()):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret == True:
 
-    # Use Canny edge detector to find edges in the image
-    edges = cv2.Canny(gray, 50, 150, None, 3)
-    cv2.imshow("edges", edges)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('gray', gray)
 
-    kernel = np.ones((10, 10), np.uint8)
-    erode = cv2.erode(edges, kernel)
+        # Use Canny edge detector to find edges in the image
+        edges = cv2.Canny(gray, 50, 150, None, 3)
+        cv2.imshow("edges", edges)
 
-    final = cv2.cvtColor(erode, cv2.COLOR_GRAY2BGR) 
+        kernel = np.ones((10, 10), np.uint8) 
+        erode = cv2.erode(edges, kernel)
 
-    # Use Hough Line Transform to detect lines in the image
-    rho = 1  # distance resolution in pixels of the Hough grid
-    theta = np.pi / 180  # angular resolution in radians of the Hough grid
-    threshold = 30  # minimum number of votes (intersections in Hough grid cell)
-    min_line_length = 50  # minimum number of pixels making up a line
-    max_line_gap = 20  # maximum gap in pixels between connectable line segments
-    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-                        min_line_length, max_line_gap)
+        final = cv2.cvtColor(erode, cv2.COLOR_GRAY2BGR)
 
-    # Go through array of lines and check for pairs of parallel lines
-    if lines is not None:
-        for i in range(len(lines)):
-            for j in range(i + 1, len(lines)):
-                if parallel(lines[i], lines[j]):
-                    drawLine(lines[i])
+        # Use Hough Line Transform to detect lines in the image
+        rho = 1  # distance resolution in pixels of the Hough grid
+        theta = np.pi / 180  # angular resolution in radians of the Hough grid
+        threshold = 30  # minimum number of votes (intersections in Hough grid cell)
+        min_line_length = 50  # minimum number of pixels making up a line
+        max_line_gap = 20  # maximum gap in pixels between connectable line segments
+        lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                                min_line_length, max_line_gap)
 
-    cv2.imshow('Frame',frame)
-    cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", final)
-  
- 
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-      break
- 
-  # Break the loop
-  else: 
-    break
+        # Go through array of lines and check for pairs of parallel lines
+        if lines is not None:
+            for i in range(len(lines)):
+                for j in range(i + 1, len(lines)):
+                    if parallel(lines[i], lines[j]) and tuple(lines[i][0]) not in drawn_lines:
+                        drawLine(lines[i])
+
+        cv2.imshow('Frame', frame)
+        cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", final)
+
+        # Press Q on keyboard to exit
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    # Break the loop
+    else:
+        break
 
 # When everything done, release the video capture object
 cap.release()
- 
+
 # Closes all the frames
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows()
